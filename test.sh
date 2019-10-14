029adb69bd07e4eec6370dbd4e2ddd04d3d8f415
Roman Marlovits <roman.marlovits@gmail.com>
2019-10-14 16:35:58 +0200

    version

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo $DIR
+
+API_URL="https://hub.docker.com/v2"
+USERNAME=romarius75
+PASSWORD=M2sZS15uD13
+
+echo "GET TOKEN"
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${USERNAME}'", "password": "'${PASSWORD}'"}' ${API_URL}/users/login/ | sed -e 's/.*"token": "\(.*\)".*/\1/')
+echo $TOKEN
+
+curl -s -H "Authorization: JWT ${TOKEN}" --data-urlencode full_description@README.md ${API_URL}/repositories/rm-ticketing-node-server/
+
+#echo "UPDATE DOCKERHUB"
+#RESPONSE=$(curl -s --write-out %{response_code} --output /dev/null -H "Authorization: JWT ${TOKEN}" -X PATCH --data-urlencode full_description@README.md ${API_URL}/repositories/rm-ticketing-node-server/)
+#echo $RESPONSE
+
